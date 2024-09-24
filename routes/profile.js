d82009fd46fc20d39083d98fe0359c2f19f5ac9d
AnuragRawat92 <148265902+AnuragRawat92@users.noreply.github.com>
2024-09-24 21:46:37 +0530

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -35,6 +35,15 @@ router.post('/', async (req, res) => {
     res.status(500).json({ message: 'Error saving profile', error });
   }
 });
-
+//  API to get LinkedIn profile data
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find(); 
+    res.status(200).json(profiles);
+  } catch (error) {
+    console.error('Error fetching profiles:', error);
+    res.status(500).json({ message: 'Error fetching profiles', error });
+  }
+});
 module.exports = router; // Exporting the router for use in other parts of the application
 
